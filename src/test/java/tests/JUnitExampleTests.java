d4d317bc57787ff13c7e93fdd38b8442e4c1eff0
KovalyovB <thabuz555@gmail.com>
2026-06-04 00:09:00 +0300

    update comment in test method

--- src/test/java/tests/JUnitExampleTests.java
+++ src/test/java/tests/JUnitExampleTests.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class JUnitExampleTests {
     @BeforeAll
     static void startup() {
-        System.out.println("Startup tests");
+        System.out.println("Startup testsNEW");
     }
 
     @AfterAll
