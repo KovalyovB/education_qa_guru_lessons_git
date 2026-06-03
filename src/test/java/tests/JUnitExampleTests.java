3dfdf61f583cbff07d567eb433ff9d033fa460a6
KovalyovB <thabuz555@gmail.com>
2026-06-04 00:01:37 +0300

    reorganize project structure v.2.0

--- src/test/java/tests/JUnitExampleTests.java
+++ src/test/java/tests/JUnitExampleTests.java
@@ -1,4 +1,10 @@
-package com.kovalyov;
+package tests;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 public class JUnitExampleTests {
